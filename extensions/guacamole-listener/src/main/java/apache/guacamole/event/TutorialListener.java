4676ad3a526e9bc71731a7a84662c4e72580c227
Saran <saran@securden.com>
2024-06-11 18:00:19 +0530

    api request to primary in event listener done

--- extensions/guacamole-listener/src/main/java/apache/guacamole/event/TutorialListener.java
+++ extensions/guacamole-listener/src/main/java/apache/guacamole/event/TutorialListener.java
@@ -1,19 +1,13 @@
 package org.apache.guacamole.event;
 
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 import org.apache.guacamole.GuacamoleException;
 import org.apache.guacamole.net.event.TunnelCloseEvent;
 import org.apache.guacamole.net.event.TunnelConnectEvent;
 import org.apache.guacamole.net.event.listener.Listener;
-import org.json.JSONObject;
-
-import com.mashape.unirest.http.HttpResponse;
-import com.mashape.unirest.http.JsonNode;
-import com.mashape.unirest.http.Unirest;
-import com.mashape.unirest.http.exceptions.UnirestException;
-
-// import org.slf4j.Logger;
-// import org.slf4j.LoggerFactory;
-
 /**
  * A Listener implementation intended to demonstrate basic use
  * of Guacamole's listener extension API.
@@ -28,34 +22,40 @@ public class TutorialListener implements Listener {
         System.out.println("Inside Handle Event");
         System.out.println("EVENT TYPE :: " + event.getClass().getName());
         if (event instanceof TunnelConnectEvent) {
-            System.out.println("UUID :: " + String.valueOf(((TunnelConnectEvent) event)
+            System.out.println("TunnelConnectEvent UUID :: " + String.valueOf(((TunnelConnectEvent) event)
               .getTunnel().getUUID()));
         }
         if (event instanceof TunnelCloseEvent) {
-            System.out.println("UUID :: " + String.valueOf(((TunnelCloseEvent) event)
+            System.out.println(" TunnelCloseEvent  UUID :: " + String.valueOf(((TunnelCloseEvent) event)
             .getTunnel().getUUID()));
-            sendAPIRequest(event.getClass().getName(), String.valueOf(((TunnelCloseEvent) event).getTunnel().getUUID()));
+            sendAPIRequest(String.valueOf(((TunnelCloseEvent) event).getTunnel().getUUID()));
         }
         
         // logger.info("received Guacamole event notification");
     }
 
-    public void sendAPIRequest(String event_type, String UUID){
-        String primary_url = "http://192.168.11.84:8000";
-        JSONObject requestBody = new JSONObject().put("tunnel_id",UUID);
-
-
+    public void sendAPIRequest(String UUID){
         try {
-            HttpResponse<JsonNode> response = Unirest.post(primary_url+"/accountmanagement/web_rdp_tunnel_id")
-            .header("Content-Type", "application/json")
-            .body(requestBody)
-            .asJson();
-            if(response.getStatus() == 200) {
-                System.out.println("Request to Primary successful");
-            } else{
-                System.out.println("Request to Primary Failed");
+            System.out.println("sendAPIRequest -->" + UUID);
+            String jsonPayload = "{\"tunnel_id\": \"" + UUID + "\"}";
+            URL url = new URL("http://192.168.11.84:8000/accountmanagement/web_rdp_tunnel_id");
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Content-Type", "application/json; utf-8");
+            connection.setRequestProperty("Accept", "application/json");
+            connection.setDoOutput(true);
+
+            
+            try (OutputStream os = connection.getOutputStream()) {
+                byte[] input = jsonPayload.getBytes("utf-8");
+                os.write(input, 0, input.length);
             }
-        } catch (UnirestException e) {
+
+            int responseCode = connection.getResponseCode();
+            System.out.println(responseCode);
+            connection.disconnect();
+        } 
+        catch (Exception e) {
             e.printStackTrace();
         }
     }
